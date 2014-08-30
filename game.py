c241f279bd0524726f21a43969ec2cec75279a31
David Raison <david@tentwentyfour.lu>
2014-08-30 15:38:47 +0200

    Added empty init file to support folder

--- game.py
+++ game.py
@@ -6,15 +6,23 @@ Avocados and stuff
 import os, random
 import pygame
 from pygame.locals import *
+
 from support import colors
 import hud
+
+# Move this outside
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width,screen_height))
+bg = (0,0,0)
+screen.fill(bg)
 
 def main():
     pygame.init()
     pygame.display.set_caption('Avocados')
     clock = pygame.time.Clock()
 
-
+    running = True
     while running:
         # Limit to 50 fps
         time_passed = clock.tick(30)
